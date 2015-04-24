356ab2588b2645136e7568e20589f3cf506ae020
Chris <hello@maxxdev.de>
2015-04-24 11:51:32 +0200

    sharing in den einzelnen beiträgen auf stage

--- wp-content/themes/gti/page-stage.php
+++ wp-content/themes/gti/page-stage.php
@@ -1,4 +1,5 @@
 <?php $slides = RNRA_Stage::loadSlides(); ?>
+<?php $tumblr_blog_url = "http://igiwuhesa487.tumblr.com" ?>
 
 <div class="slider">
 	<?php foreach ($slides as $slide): ?>
@@ -37,16 +38,39 @@
 <div class="articles">
 	<?php foreach ($slides as $slide): ?>
 		<?php $post = get_post($slide->ID) ?>
-		<article class="" id="<?php echo $post->post_name ?>">
+		<article class="article" id="<?php echo $post->post_name ?>" data-article="<?php echo $post->post_name ?>">
 			<div class="row">
 				<div class="small-12 columns closewrapper">
 					<a href="#" class="close-article">
 						Schließen <i class="fa fa-times"></i>
 					</a>
 				</div>
-				<div class="small-12 columns">
+				<div class="small-12 medium-5 columns">
 					<h2><?php echo $post->post_title ?></h2>
 					<?php echo $post->post_content ?>
+				</div>
+				<div class="small-12 medium-7 columns">
+					<div class="share">
+						<strong>Teile dies</strong>
+						<div class="sharewrapper">
+							<ul class="socialmedia">
+								<li class="facebook">
+									<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $tumblr_blog_url ?>%23<?php echo $post->post_name ?>"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li class="twitter">
+									<a href="https://twitter.com/home?status=<?php echo $tumblr_blog_url ?>%23<?php echo $post->post_name ?>"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li class="pinterest">
+									<a href="https://pinterest.com/pin/create/button/?url=<?php echo $tumblr_blog_url ?>%23<?php echo $post->post_name ?>&amp;media="><i class="fa fa-pinterest-p"></i></a>
+								</li>
+							</ul>
+						</div>
+						<div class="share-meta">
+							<span class="note"></span>
+							<span class="date"></span>
+						</div>
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa nam quos ipsa tempore veritatis, libero temporibus atque magni et, laboriosam qui pariatur porro veniam perspiciatis! Sed natus enim quaerat voluptate.</p>
 				</div>
 			</div>
 		</article>
